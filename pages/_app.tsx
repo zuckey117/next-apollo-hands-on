e3f1481ab331e1cffd6466b5ceaba93473241a53
zuckey <katsuki@pepabo.com>
2021-12-01 12:43:54 +0900

    apollo clientの設定

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,23 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import "../styles/globals.css";
+import type { AppProps } from "next/app";
+import { ApolloClient, ApolloProvider, InMemoryCache } from "@apollo/client";
 
+import "../styles/globals.css";
+
+const client = new ApolloClient({
+  uri: "https://api.github.com/graphql",
+  headers: {
+    Authorization: `bearer ${process.env.NEXT_PUBLIC_GITHUB_TOKEN}` || "",
+  },
+  ssrMode: typeof window === "undefined",
+  cache: new InMemoryCache(),
+});
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <ApolloProvider client={client}>
+      <Component {...pageProps} />
+    </ApolloProvider>
+  );
 }
 
-export default MyApp
+export default MyApp;
