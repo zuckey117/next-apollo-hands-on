8d095520dfa91d77389f64e3e0934aab6a50d349
zuckey <katsuki@pepabo.com>
2021-12-02 19:40:19 +0900

    リポジトリ検索のページ実装

---
+++ pages/repositories.tsx
@@ -0,0 +1,47 @@
+import { useState } from 'react'
+import { useSearchRepositoriesQuery, Repository } from '../lib/generated/graphql'
+
+const Repositories = () => {
+  const [keyword, setKeyword] = useState('')
+
+  const { loading, error, data } = useSearchRepositoriesQuery({ variables: { keyword } })
+  if (error) {
+    return <p>{error.message}</p>
+  }
+  return (
+    <main>
+      <h1>リポジトリ検索</h1>
+      <label>
+        キーワード
+        <input
+          type="text"
+          value={keyword}
+          onChange={(event) => {
+            setKeyword(event.target.value)
+          }}
+        />
+      </label>
+      
+      {loading ? (
+        <h2>読込中.....</h2>
+      ) : (
+        <>
+          <h2>検索結果</h2>
+          <div>
+            {data?.search.nodes?.map((node) => {
+              // GraphQLのUnion型の型がうまくTSの型に変換されなかったので苦肉のas....ごめんなさい...
+              const repository = node as Repository
+              return (
+                <a key={repository.id} href={repository.url}>
+                  <p>{repository.url}</p>
+                </a>
+              )
+            })}
+          </div>
+        </>
+      )}
+    </main>
+  )
+}
+
+export default Repositories
