22c5a1bda3f24b413589ed785dd57101c64f1da5
zuckey117 <soichiro.katsuki@gmail.com>
2022-09-27 23:25:08 +0900

    無限ループが発生する例

---
+++ pages/next-hands-on/use-effect-infinite-loop.tsx
@@ -0,0 +1,27 @@
+import { FC, useEffect, useState } from 'react'
+
+const UseEffectInfiniteLoop: FC = () => {
+  const [value, setValue] = useState<{ key1: string | null; key2: string | null } | null>(null)
+  useEffect(() => {
+    window.localStorage.setItem('key1', 'value1')
+    window.localStorage.setItem('key2', 'value2')
+  }, [])
+  
+  const getLocalStorageValue = (key: string) => {
+    return window.localStorage.getItem(key)
+  }
+
+  useEffect(() => {
+    console.log('useEffect is called')
+    setValue({ key1: getLocalStorageValue('key1'), key2: getLocalStorageValue('key2') })
+  }, [getLocalStorageValue])
+  return (
+    <>
+      <div>useEffect inifinite loop</div>
+      <p>key1: {value?.key1}</p>
+      <p>key2: {value?.key2}</p>
+    </>
+  )
+}
+
+export default UseEffectInfiniteLoop
