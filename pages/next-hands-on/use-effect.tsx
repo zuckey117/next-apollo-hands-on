dc4ceaca1bacbb2aa23922d2ab2fcfb3d828a685
zuckey117 <soichiro.katsuki@gmail.com>
2022-09-27 22:17:55 +0900

    依存配列ありのuseEffectの動作確認

--- pages/next-hands-on/use-effect.tsx
+++ pages/next-hands-on/use-effect.tsx
@@ -1,11 +1,31 @@
-import { FC, useEffect } from 'react'
+import { FC, useEffect, useState } from 'react'
 
 const UseEffect: FC = () => {
+  const [postCode, setPostCode] = useState('')
   console.log('this log is run on both client and server side. ')
   useEffect(() => {
     console.log('this log is run only client.')
   })
-  return <div>useEffect hands on</div>
+  useEffect(() => {
+    console.log('post code is changed.')
+    if (postCode.length === 7)
+      fetch(`https://zipcloud.ibsnet.co.jp/api/search?zipcode=${postCode}`)
+        .then((response) => {
+          return response.json()
+        })
+        .then((data) => {
+          alert(data.results[0].address1 + data.results[0].address2 + data.results[0].address3)
+        })
+  }, [postCode])
+  return (
+    <>
+      <div>useEffect hands on</div>
+      <label>
+        郵便番号
+        <input type="text" value={postCode} onChange={(e) => setPostCode(e.currentTarget.value)} />
+      </label>
+    </>
+  )
 }
 
 export default UseEffect
